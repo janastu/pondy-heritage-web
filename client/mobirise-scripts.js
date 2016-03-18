489d4d863eca9afaca9a276ded9324dc1e23a93c
salus-sage <salus.sage@gmail.com>
2016-03-18 19:20:40 +0530

    minor bug fic

--- client/mobirise-scripts.js
+++ client/mobirise-scripts.js
@@ -4,8 +4,10 @@ Template.navBar.onRendered(function(){
  $.getScript('assets/smooth-scroll/SmoothScroll.js', function(){
   $.getScript('assets/bootstrap-carousel-swipe/bootstrap-carousel-swipe.js', function() {
     $.getScript('assets/mobirise/js/script.js', function(){
+    	
         // script has loaded
   		console.log('conectaReady', "loaded");
+  	
     });
   });
   
