fc6b5efa07d6c68724dff625056d98fd7a36c1bf
salus-sage <salus.sage@gmail.com>
2016-04-18 17:13:21 +0530

    Adding dynamic routes

--- client/routes.js
+++ client/routes.js
@@ -2,44 +2,58 @@
 
 FlowRouter.route('/', {
     action: function(params) {
-        FlowLayout.render('pondyHome');
-    }
+        FlowLayout.render('mainLayout', {content: 'pondyHome'});
+    },
+    name: 'home.show'
+});
+FlowRouter.route('/apps/:appId', {
+    action: function(params) {
+        FlowLayout.render('mainLayout', {content: 'group'});
+    },
+    name: 'rest'
 });
 FlowRouter.route('/about', {
     action: function(params) {
-        FlowLayout.render('about');
-    }
+        FlowLayout.render('mainLayout', {content:'about'});
+    },
+    name: 'about'
 });
 FlowRouter.route('/instructions', {
     action: function(params) {
-        FlowLayout.render('Instructions');
-    }
+        FlowLayout.render('mainLayout', {content:'Instructions'});
+    },
+    name: 'instructions'
 });
 FlowRouter.route('/contact', {
     action: function(params) {
-        FlowLayout.render('contact');
-    }
+        FlowLayout.render('mainLayout', {content:'contact'});
+    },
+    name: 'contact'
 });
 FlowRouter.route('/download', {
     action: function(params) {
-        FlowLayout.render('Download');
-    }
+        FlowLayout.render('mainLayout', {content:'Download'});
+    },
+    name: 'download'
 });
 FlowRouter.route('/map', {
     action: function(params) {
-        FlowLayout.render('Layout', {content: 'Map'});
-    }
+        FlowLayout.render('mainLayout', {content: 'Map'});
+    },
+    name: 'map'
 });
   FlowRouter.route('/login', {
     action: function(params) {
-        FlowLayout.render('Login');
+        FlowLayout.render('mainLayout', {content:'Login'});
         Session.set('showDialog', false);
-    }
+    },
+      name: 'login'
 });
 FlowRouter.route('/register', {
     action: function(params) {
-        FlowLayout.render('Register');
-    }
+        FlowLayout.render('mainLayout', {content:'Register'});
+    },
+    name: 'register'
 });
 /*FlowRouter.route('/add-to-map', {
     action: function(params) {
